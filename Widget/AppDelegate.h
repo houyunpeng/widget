c6f6cdf73e7c8cb3cdce2b5018d14e9fc57936de
hyp <hyp@hypdemac-mini.local>
2016-01-07 10:12:58 +0800

    Initial Commit

---
+++ Widget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Widget
+//
+//  Created by hyp on 16/1/7.
+//  Copyright © 2016年 hyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Widget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Widget
+//
+//  Created by hyp on 16/1/7.
+//  Copyright © 2016年 hyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
