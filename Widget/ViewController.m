b0f18a4949caa96a2d74c4a182feec7d9a567040
houyunpeng <1600892327@qq.com>
2016-01-19 15:55:48 +0800

    a luge of messages

--- Widget/ViewController.m
+++ Widget/ViewController.m
@@ -19,6 +19,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+// add a luge of messages
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
