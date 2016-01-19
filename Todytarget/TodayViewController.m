fd0ffd1181a6574b3281523d4cc2ee83f30d5a5c
houyunpeng <1600892327@qq.com>
2016-01-19 15:44:39 +0800

    modi .m

--- Todytarget/TodayViewController.m
+++ Todytarget/TodayViewController.m
@@ -17,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib master.
+    // Do any additional setup after loading the view from its nib master 1243234.
     
     NSLog(@"------frame----%@",NSStringFromCGRect(self.view.frame));
 //    self.view.backgroundColor = [UIColor whiteColor];
